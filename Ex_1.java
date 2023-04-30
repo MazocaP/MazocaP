1f3fc3a13849c2584d06729cede9863cb831ea78
Mateus Augusto do Prado <105027659+MazocaP@users.noreply.github.com>
2023-04-30 14:36:20 -0300

    Create Ex_1.java

---
+++ Ex_1.java
@@ -0,0 +1,13 @@
+package ex.pkg1;
+public class Ex1 {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        a1.matricula = 1;
+        a1.nome = "Joao";
+        a1.notap1 = 5.5;
+        a1.notap2 = 9.0;
+        a1.notat = 6;
+        a1.media();
+        a1.passou();
+    }
+}
